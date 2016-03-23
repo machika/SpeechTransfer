39c3373f0a356cab288c6ae1388e4d132580809b
machi <machi0545@hotmail.com>
2016-03-23 19:16:24 +0900

    Add predefined speech buttons

--- app/src/main/java/com/example/machi/speechtransfer/MainActivity.java
+++ app/src/main/java/com/example/machi/speechtransfer/MainActivity.java
@@ -5,23 +5,40 @@ import android.content.Intent;
 import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private final static int ICode = 0;
+    private final static String SPEECH_SERVER = "192.168.0.254";
+    private final static int SPEECH_SERVER_PORT = 10080;
+    private final static String LOG_TAG = "SpeechTransfer";
 
     @Override
     public void onClick(View view) {
-        try {
-            Intent it=new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-            it.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-            it.putExtra(RecognizerIntent.EXTRA_PROMPT,"しゃべったことをRaspberryPiに送信するよ！");
-            it.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS,1);
-            startActivityForResult(it,ICode);
-        }  catch (ActivityNotFoundException e) {
+        if (view.getId() == R.id.speechButton) {
+            try {
+                Intent it = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                it.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                it.putExtra(RecognizerIntent.EXTRA_PROMPT, "しゃべったことをRaspberryPiに送信するよ！");
+                it.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 1);
+                startActivityForResult(it, ICode);
+            } catch (ActivityNotFoundException e) {
+                Log.e(LOG_TAG, "Speech recognize app doesn't exist!");
+            }
+        }
+        else {
+            Button pushedButton = (Button)view;
+            sendPostRequest(pushedButton.getText().toString(), SPEECH_SERVER, SPEECH_SERVER_PORT);
         }
     }
 
@@ -36,6 +53,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             //TODO: Send this text to Raspberry pi (run some server)
             EditText myEditText = (EditText)findViewById(R.id.speechText);
             myEditText.setText(msg);
+
+            boolean result = sendPostRequest(msg, SPEECH_SERVER, SPEECH_SERVER_PORT);
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
@@ -46,5 +65,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         findViewById(R.id.speechButton).setOnClickListener(this);
+        findViewById(R.id.goodmorning).setOnClickListener(this);
+        findViewById(R.id.hello).setOnClickListener(this);
+        findViewById(R.id.goodnight).setOnClickListener(this);
+    }
+
+    private boolean sendPostRequest(String msg, String url, int port) {
+        boolean result = true;
+        HttpURLConnection conn = null;
+        try {
+            conn = (HttpURLConnection) new URL("http", url, port, "").openConnection();
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+            conn.setRequestProperty("Content-Type", "text/plain; charset=UTF-8");
+            conn.connect();
+            Log.i(LOG_TAG, "connected: server=" + url + ", port=" + String.valueOf(port));
+
+            DataOutputStream os = new DataOutputStream(conn.getOutputStream());
+            os.write(msg.getBytes("UTF-8"));
+            os.flush();
+            os.close();
+
+            if( conn.getResponseCode() != HttpURLConnection.HTTP_OK ){
+                Log.e(LOG_TAG, "Server returned error code " + String.valueOf(conn.getResponseCode()));
+                result = false;
+            }
+        }catch(IOException e){
+            Log.e(LOG_TAG, "Got exception during sendPostRequest(): " + e.getMessage());
+            result = false;
+        }finally {
+            if( conn != null ){
+                conn.disconnect();
+            }
+        }
+        return result;
     }
 }
